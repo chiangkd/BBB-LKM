e7a3fc405269c2ce40cafd959ca9f730a9e028e8
aaron <chiangkuandi@gmail.com>
2025-08-14 23:07:08 +0800

    20250814

--- 002_pseudo/pseudo.c
+++ 002_pseudo/pseudo.c
@@ -46,20 +46,17 @@ static loff_t pseudo_lseek(struct file *file, loff_t offset, int orig)
 			break;
 		default:
 			return -EINVAL;
-
 		if (new_pos > MEM_SIZE) {
 			new_pos = MEM_SIZE;
 		}
-
 		if (new_pos < 0) {
 			new_pos = 0;
 		}
-
 		file->f_pos = new_pos;
 	}
-
 	return new_pos;
 }
+
 static ssize_t pseudo_read(struct file *file, char __user *buf, size_t size, loff_t *offset)
 {
 	pr_info("read requested for %zu bytes\n", size);
@@ -101,6 +98,7 @@ static ssize_t pseudo_write(struct file *file, const char __user *buf, size_t si
 	}
 
 	if (size == 0) {
+		pr_err("No space left on the device\n");
 		return -ENOMEM;
 	}
 
@@ -142,27 +140,56 @@ struct file_operations pseudo_fops = {
 
 static int __init pseudo_init(void)
 {
+	int rc = 0;
+
     // Dynamically allocate a device number
-    alloc_chrdev_region(&device_number, 0, 1, PSEUDO_DEIVCE_NAME);
+    rc = alloc_chrdev_region(&device_number, 0, 1, PSEUDO_DEIVCE_NAME);
+
+	if (rc < 0) {
+		pr_err("alloc_chrdev_region fail\n");
+		goto alloc_cdev_fail;
+	}
 
 	pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
     // Initialize the cdev structure with fops
-    cdev_init(&pseudo_cdev, &pseudo_fops);
+	cdev_init(&pseudo_cdev, &pseudo_fops);
 
     // Register a device (cdev structure) with VFS
     pseudo_cdev.owner = THIS_MODULE;
-    cdev_add(&pseudo_cdev, device_number, 1);
-
+	rc = cdev_add(&pseudo_cdev, device_number, 1);
+	if (rc < 0) {
+		pr_err("cdev_add fail\n");
+		goto unreg_cdev;
+	}
     // Create device class under /sys/class
     pseudo_class = class_create(THIS_MODULE, PSEUDO_DEIVCE_NAME);
+	if (IS_ERR(pseudo_class)) {
+		pr_err("class_create fail\n");
+		rc = PTR_ERR(pseudo_class);
+		goto cdev_del;
+	}
 
     // Populate the sysfs with device information
     pseudo_device = device_create(pseudo_class, NULL, device_number, NULL, PSEUDO_DEIVCE_NAME);
+	if (IS_ERR(pseudo_device)) {
+		pr_err("device_create fail\n");
+		rc = PTR_ERR(pseudo_device);
+		goto class_del;
+	}
 
 	pr_info("Module init was successful\n");
 
-    return 0;
+    return rc;
+
+class_del:
+	class_destroy(pseudo_class);
+cdev_del:
+	cdev_del(&pseudo_cdev);
+unreg_cdev:
+	unregister_chrdev_region(device_number, 1);
+alloc_cdev_fail:
+	return rc;
 }
 
 static void __exit pseudo_exit(void)
