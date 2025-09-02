1c77b64dcba2c8f5f2db7c6167769b7c0cff28c2
aaron <chiangkuandi@gmail.com>
2025-09-03 02:04:50 +0800

    20250903

--- 004_pseudo_platform_driver/pseudo_driver.c
+++ 004_pseudo_platform_driver/pseudo_driver.c
@@ -4,6 +4,9 @@
 #include <linux/device.h>
 #include <linux/kdev_t.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
+
+#include "platform.h"
 
 #define NO_OF_DEVICES 4
 
@@ -31,7 +34,7 @@ struct pseudo_drv_private_data {
 
 // Device private data structure
 struct pseudo_dev_private_data {
-	struct pseudo_drv_private_data pdata;
+	struct pseudo_platform_data pdata;
 	char *buffer;
 	dev_t dev_num;
 	struct cdev pseudo_cdev;
@@ -118,8 +121,51 @@ int pseudo_platform_driver_remove(struct platform_device *pdef)
 // Gets called when matched platform device is found
 int pseudo_platform_driver_probe(struct platform_device *pdev)
 {
+	int rc = 0;
+
+	struct pseudo_dev_private_data *device_data;
+	struct pseudo_platform_data *pdata;
+	/** 1. Get the platform data */
+	pdata = (struct pseudo_platform_data *) dev_get_platdata(&pdev->dev);
+	if (!pdata) {
+		pr_info("No platform data available\n");
+		rc = -EINVAL;
+		goto out;
+	}
+	/** 2. Dynamically allocate memory for the device private data */
+	device_data = (struct pseudo_dev_private_data *) kzalloc(sizeof(struct pseudo_dev_private_data), GFP_KERNEL);
+	if (!device_data) {
+		pr_info("Cannot allocate memory\n");
+		rc = -ENOMEM;
+		goto out;
+	}
+
+	device_data->pdata.size = pdata->size;
+	device_data->pdata.perm = pdata->perm;
+	device_data->pdata.serial_number = pdata->serial_number;
+	
+	pr_info("Device serial number = %s\n", pdata->serial_number);
+	pr_info("Device size = %d\n", pdata->size);
+	pr_info("Device permission = %d\n", pdata->perm);
+
+	/** 3. Dynamically allocate memory for the device buffer using suze
+	 * information from the platform data */
+
+	/** 4. Get the devuce number */
+
+	/** 5. Do cdev init and cdev add */
+
+	/** 6. Create device file for the detected platform device */
+
+	/** 7. Error handling */
+
+	
     pr_info("A device is detected\n");
     return 0;
+
+out:
+	pr_info("DEvice probe failed\n");
+	return rc;
 }
 
 
